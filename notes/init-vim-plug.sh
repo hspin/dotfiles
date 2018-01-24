c172c24d77598a8da60e8bcfe66cdc304ef43984
GS Blackice <me.cloudvm@hsx5.com>
2018-01-24 15:05:56 -0500

    zdev update scripts

---
+++ notes/init-vim-plug.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# bash-local
+# export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+# export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
+# bash-local
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- zdev/init-npm-pretty.sh
+++ zdev/init-npm-pretty.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# bash-local
-# export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
-# export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
-# bash-local
-
 npm install --only=dev eslint babel-eslint eslint-plugin-react
 npm install --only=dev prettier eslint-config-prettier eslint-plugin-prettier
 
